b44ae05b7868b3c177a33c971d05e53a7b88892a
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-03-17 21:38:20 +0100

    Save to file

--- js/parser.js
+++ js/parser.js
@@ -1,21 +1,6 @@
+const fs = require('fs')
 const getline = require('readline-sync')
 const { ask, askForCriterias, askForAlternatives, makeMatrix } = require('./functions')
-
-// const goal = 'Car'
-// const tree = {
-//     alternatives: ['Tesla', 'Mercedes'],
-//     Car: {
-//         matrix: [],
-//         Looks: {
-//             matrix: [],
-//             Color: [],
-//             Wheels: []
-//         },
-//         Speed: [],
-//         Cost: []
-//     }
-// }
-// const alternatives = ['Tesla', 'Mercedes']
 
 const tree = {
     alternatives: [],
@@ -26,25 +11,9 @@ const howMany = getline.question('How many criterias does it have? ')
 tree[goal] = { matrix: makeMatrix(howMany), }
 
 const alternatives = askForAlternatives(tree)
+
 askForCriterias(tree[goal], alternatives)
+
 ask(tree[goal], goal, alternatives)
 
-console.log(tree)
-
-// const mainAlternatives = tree.alternatives
-// const goal = Object.keys(tree).filter(it => it !== 'alternatives')[]
-
-// console.log(tree['Goal'].matrix)
-// console.log(tree['Goal']['Criterion 1'].matrix)
-// console.log(tree['Goal']['Criterion 1']['Sub-criterion 1'])
-// console.log(tree['Goal']['Criterion 1']['Sub-criterion 2'])
-// console.log(tree['Goal']['Criterion 1']['Sub-criterion 3'])
-// console.log(tree['Goal']['Criterion 1']['Sub-criterion 4'])
-// console.log(tree['Goal']['Criterion 2'].matrix)
-// console.log(tree['Goal']['Criterion 2']['Sub-criterion 4'])
-// console.log(tree['Goal']['Criterion 2']['Sub-criterion 5'])
-// console.log(tree['Goal']['Criterion 2']['Sub-criterion 6'])
-// console.log(tree['Goal']['Criterion 3'].matrix)
-// console.log(tree['Goal']['Criterion 3']['Sub-criterion 7'])
-// console.log(tree['Goal']['Criterion 3']['Sub-criterion 8'])
-// console.log(tree['Goal']['Criterion 3']['Sub-criterion 9'])
+fs.writeFileSync(`./${goal}.json`, JSON.stringify(tree))
